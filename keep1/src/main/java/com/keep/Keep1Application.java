848f419cbc9383b7126150593b43772949af46a0
z12999941 <z12999941@999941-BAEHY.poscoplantec.co.kr>
2025-04-23 16:59:53 +0900

    1

---
+++ keep1/src/main/java/com/keep/Keep1Application.java
@@ -0,0 +1,13 @@
+package com.keep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Keep1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Keep1Application.class, args);
+	}
+
+}
